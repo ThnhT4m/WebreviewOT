b83c4c4b1927ecf457c90b27e9254737aa0b3103
ThnhT4m <thanhtamdz.2210@gmail.com>
2024-11-26 02:14:07 +0700

    no fixed demo

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("learn-more-btn").addEventListener("click", function () {
+    alert("The Grand Arena hosts international tournaments and iconic events. Book your tickets now!");
+  });
+  
