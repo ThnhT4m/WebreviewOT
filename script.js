49997c29768f07d51519e4094bdf8c70f3bf790b
ThnhT4m <thanhtamdz.2210@gmail.com>
2024-11-26 23:38:52 +0700

    fixed

--- script.js
+++ script.js
@@ -1,4 +1,57 @@
-document.getElementById("learn-more-btn").addEventListener("click", function () {
-    alert("The Grand Arena hosts international tournaments and iconic events. Book your tickets now!");
-  });
-  
+function scrollToOverview() {
+  document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+}
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+
+
+next.onclick = function () {
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+};
+
+
+prev.onclick = function () {
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+};
+
+
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 3000);
+
+
+function reloadSlider() {
+    slider.style.left = -items[active].offsetLeft + 'px';
+
+
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 3000);
+}
+
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', () => {
+        active = key;
+        reloadSlider();
+    });
+});
+
+
+window.onresize = function () {
+    reloadSlider();
+};
